d8384b2b13721e52076546d1eefdd8f72e6f7c90
sqnzhqr <karamashj@gmail.com>
2024-03-04 18:08:15 +0600

    add products feature

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     path('profile/', views.ProfileView.as_view(), name='profile'),
     path('products/<int:pk>', views.products_list, name='products'),
+    path('products/add', views.add_product, name='add_product'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
